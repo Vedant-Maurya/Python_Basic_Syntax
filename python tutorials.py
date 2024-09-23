564127d3b9e2700dc78e0fe1824b0adbdcbb1691
Vedant-Maurya <vedant7052@gmail.com>
2024-09-23 01:14:20 -0700

    learning purpose python syntax

---
+++ python tutorials.py
@@ -0,0 +1,207 @@
+#assignment operators
+#arithemetic operators
+#comparison operators
+#logicaloperators
+#bitwise opetrator
+#membership operators
+#identity operators
+
+#assignment operators= +=.-=,*=,/=,%=,**=
+#arithemetic operators= +,-,*/,**,%...floor divison it gibe he diveisor of the senttyenso
+exponents =and it give the remainder of the codes
+
+#comparison operators= ==,!=,<,> ,<= ,>=
+#logical operators=and not or
+#bitwise operoators= and or xor not << >>
+#membership operator= in ,not in
+#identity operators=is ,is not
+
+
+modulus = Remainder %
+exponents = Square **
+floor division = divider //
+division = /
+
+
+
+a=5
+b=6
+var=5==6
+print(var)
+
+
+a=5
+b=6
+var=5is not6
+print(var)
+
+
+a=5
+b=6
+var=5<=6
+print(var)
+
+
+a=6
+b=6
+var=a!=b
+print(var)
+
+'''---------------------------------------------------------------------'''
+
+''operators
+''data types
+-----------------------------------
+assignments
+arithementics
+logicals
+comparison
+identity
+membership
+bitwise
+
++=,-=,*=,/=,//=,**=,%=
++,-,*,/,%,//,**,
+and or not
+==,<,>,!=,>=,<==
+is is not
+in not in
+
+string   ""
+integers 123
+set     {"",""}
+dic      {"":,"":}
+typle    ("","")
+list      ["",""]
+booleans   True,False
+none types None types
+float       50.60
+complex       20j
+
+
+'''--------------------------------------------------------------------------'''
+
+#comparison operators
+#==
+#!=
+#>=
+#<=
+#<
+#>
+
+#assignment operators
++=    addition is equal to 
+-=   substraction is equal to 
+*=   multiplicatio is equal to 
+/=    division is equal to 
+**=   exponents is qual to
+//=   floor division is equal to 
+%=   modulus is equal to
+
+#arithmetics operators
++   addition
+-    substraction
+*   multiplication
+/   division
+//  floor division
+%   modulus
+**  exponents
+
+#logical operators
+and it give the true because bothe condition of the true
+or  it give the true but at least once condition is true
+not it is used to reverse the value in the case of the true and false 
+
+#identity operators
+is  it give the value of the all statements is true
+is not  but statement is true then is will give the value of the false 
+
+#membership operators
+in  it will gets the value of the in list sequences
+not in  but these are give the value are not list in sequences
+
+
+
+
+string
+booleans
+interger
+list  []  and the commons separated value is called the list
+tuple () and the commons separated value is called the tuple
+dictionary  {}  colon separated value is called the dictionary
+set
+float
+complex
+nonetype
+
+
+a=5
+b=10
+var=a!=b
+print(var)
+
+a=25
+b=10
+var=a<b
+print(var)
+
+a=20
+b=15
+var=a>b
+print(var)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
